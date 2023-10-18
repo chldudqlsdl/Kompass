c08e092784796304f0c2f74a942d7708e08ddddf
eratchacha <papa021326@naver.com>
2023-10-19 04:10:25 +0900

    [Feat#4] Implement View Structure

---
+++ Orum/Orum/Views/DepartRemainingTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  DepartRemainingTimeView.swift
+//  Orum
+//
+//  Created by 차차 on 10/19/23.
+//
+
+import SwiftUI
+
+struct DepartRemainingTimeView: View {
+    var body: some View {
+        Text("출발까지 남은 시간 표시")
+    }
+}
+
+#Preview {
+    DepartRemainingTimeView()
+}
