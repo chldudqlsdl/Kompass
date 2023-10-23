4b3a2e75827c2d5fb64ef4fa3e0e31f1d29a8921
eratchacha <papa021326@naver.com>
2023-10-24 00:51:10 +0900

    [Feat/#27] Add API Request Loading View

--- Orum/Orum/Views/FlightInfoSubmitView.swift
+++ Orum/Orum/Views/FlightInfoSubmitView.swift
@@ -13,6 +13,7 @@ struct FlightInfoSubmitView: View {
     @State var flightCode = ""
     @State var isFlightInfoEditViewActive = false
     @State var alertShowing = false
+    @State var isLoading = false
     @AppStorage("dep_city") var dep_city : String = ""
     @AppStorage("arr_city") var arr_city : String = ""
     @AppStorage("dep_time") var dep_time : Date = Date()
@@ -20,6 +21,7 @@ struct FlightInfoSubmitView: View {
     
     var body: some View {
         NavigationStack {
+            ZStack {
             VStack {
                 Image(systemName: "wallet.pass")
                     .foregroundColor(.blue)
@@ -31,7 +33,7 @@ struct FlightInfoSubmitView: View {
                     .font(.largeTitle)
                     .bold()
                     .padding(.bottom, 10)
-
+                
                 
                 Text("더 나은 앱의 경험을 위해 비행기의 출발 시간, 도착 시간 등의 정보를 가져올게요.")
                     .multilineTextAlignment(.center)
@@ -51,7 +53,7 @@ struct FlightInfoSubmitView: View {
                 Divider()
                 
                 HStack {
-                Text("Flight Code")
+                    Text("Flight Code")
                     
                     TextField("Enter your flight code", text: $flightCode)
                         .textFieldStyle(.plain)
@@ -63,61 +65,72 @@ struct FlightInfoSubmitView: View {
                 
                 VStack(spacing: 17) {
                     Divider()
-                        
+                    
                     
                     HStack(spacing: 15) {
-                        Button(action: {}) {
-                            HStack {
-                                Image(systemName: "wallet.pass.fill")
-                                    .foregroundColor(.black)
-                                Text("Wallet")
-                            }
-                        }
-                        .foregroundColor(.black)
-                        .padding(.horizontal, 45)
-                        .padding(.vertical, 14)
-                        .background(.white)
-                        .cornerRadius(12)
-                        .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
-                        
-                        Button("Next") {
-                            print(flightCode)
-                            flightCodeAPIManager.performRequest(flightCode) { response in
-                                if let response = response {
-                                    dep_city = response.dep_city
-                                    arr_city = response.arr_city
-                                    dep_time = response.dep_time.toDate() ?? Date()
-                                    arr_time = response.arr_time.toDate() ?? Date()
-                                    isFlightInfoEditViewActive = true
-                                    print(isFlightInfoEditViewActive)
-                                } else {
-                                    alertShowing = true
-                                    print("alert")
+                        Button(action: {
+                                print(flightCode)
+                            isLoading.toggle()
+                                flightCodeAPIManager.performRequest(flightCode) { response in
+                                    if let response = response {
+                                        dep_city = response.dep_city
+                                        arr_city = response.arr_city
+                                        dep_time = response.dep_time.toDate() ?? Date()
+                                        arr_time = response.arr_time.toDate() ?? Date()
+                                        isFlightInfoEditViewActive = true
+                                        print(isFlightInfoEditViewActive)
+                                    } else {
+                                        alertShowing = true
+                                        print("alert")
+                                    }
+                                }
+                            })
+                            {
+                                HStack {
+                                    Spacer()
+                                    
+                                    if !isLoading {
+                                        Text("Next")
+                                    }
+                                    else {
+                                        ProgressView()
+                                            .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                                    }
+                                    
+                                    Spacer()
                                 }
                             }
-                        }
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 59)
-                        .padding(.vertical, 14)
-                        .background(Color.accentColor)
-                        .cornerRadius(12)
-                        .alert(isPresented: $alertShowing) {
-                            Alert(title: Text("Alert"), message: Text("항공편 코드를 다시 한번 확인해주십시오"), dismissButton: .default(Text("닫기")))
-                        }
-                        .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 59)
+                            .padding(.vertical, 14)
+                            .background(Color.accentColor)
+                            .cornerRadius(12)
+                            .alert("Alert",isPresented: $alertShowing) {
+                                  Button("닫기", role: .cancel) {}
+                                } message: {
+                                    Text("항공편 코드를 다시 한번 확인해주십시오")
+                                }
+                                .onChange(of: alertShowing) {
+                                    if alertShowing {
+                                        isLoading.toggle()
+                                    }
+                                }
+                            .shadow(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
                     }
+                    .padding(.horizontal, 15)
                     .padding(.bottom, 24)
                 }
                 .background(
-                  Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 393, height: 129)
-                    .background(Color(red: 0.96, green: 0.96, blue: 0.96)),alignment: .top
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 393, height: 129)
+                        .background(Color(red: 0.96, green: 0.96, blue: 0.96)),alignment: .top
                 )
             }
             .navigationDestination(isPresented: $isFlightInfoEditViewActive) {
                 FlightInfoEditView().navigationBarBackButtonHidden(true)
             }
+        }
         }
         
     }
