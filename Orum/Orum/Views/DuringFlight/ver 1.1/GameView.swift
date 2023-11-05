df47c7fc98ba7e7cd629332b6c87e85dec7dccd4
eratchacha <papa021326@naver.com>
2023-11-05 16:08:44 +0900

    [Feat/#57] FlightMainView UI Implemetation

--- Orum/Orum/Views/DuringFlight/ver 1.1/GameView.swift
+++ Orum/Orum/Views/DuringFlight/ver 1.1/GameView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Divider()
+            }
+            .navigationTitle("Game")
+        }
     }
 }
 
